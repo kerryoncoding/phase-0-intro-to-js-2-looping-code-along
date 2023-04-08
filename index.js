364a2bec245828ca7f50879637a67ec99e9ecd25
Kerry Guarino <kerry.guarino@gmail.com>
2023-04-08 15:26:57 -0400

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+let cardArray = [];
+
+function writeCards(nameString, event) {
+   for (let i=0; i<nameString.length; i++){
+      let message = `Thank you, ${nameString[i]}, for the wonderful ${event} gift!`;
+      cardArray.push(message);
+   }
+   return cardArray;
+}
+
+
+function countDown(startNumber){
+   while (startNumber>=0){
+      console.log(startNumber);
+      startNumber -= 1;
+   }
+}
+
+countDown(10);
